3d2d8825607356d4cc87d0be114c01fa5f8b4506
Fariz-Isg <fariz.isgandarli@gmail.com>
2025-11-29 23:44:40 +0400

    j

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an object-oriented programming language with many features"
-str = str[13:40] + str[51:56] + str[0:6]
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[43:70] + str[103:109] + str[0:6]
 print(str)
