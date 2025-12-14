bd38814665d17be747ae36245159fd59075615a4
Fariz-Isg <fariz.isgandarli@gmail.com>
2025-12-14 20:13:31 +0400

    a

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Module that defines a read_file function"""
+
+
 def read_file(filename=""):
+    """Read a text file (UTF8) and print it to stdout
+
+    Args:
+        filename (str): The name of the file to read (default: "")
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
